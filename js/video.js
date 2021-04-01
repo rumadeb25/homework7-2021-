c2cbb9b109eef7155250c1ad65fe3444227e5f9a
rumadeb25 <55928998+rumadeb25@users.noreply.github.com>
2021-04-01 10:30:36 -0400

    Add files via upload
    commit 1

---
+++ js/video.js
@@ -0,0 +1,70 @@
+var video = document.getElementById("player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML=video.volume*100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Vintage");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Back to normal");
+	video.classList.remove("oldSchool");
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate *= .95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Faster");
+	video.playbackRate /= .95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	if (video.currentTime + 15 < video.duration) {
+		console.log("Original location" + video.currentTime);
+		video.currentTime = video.currentTime + 15;
+	}
+	else{
+		console.log("Back to begining");
+		video.currentTime = 0;
+		video.play()
+	}
+	console.log("New location " + video.currentTime);
+	
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector('#slider').value / 100;
+	document.querySelector('volume').innerHTML = (video.volume * 100) + "%"
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted == true;
+		document.querySelector('#mute').innerHTML = "Unute;"
+	}
+});
+
+
+
+
